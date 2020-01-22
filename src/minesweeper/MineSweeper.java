690c35be9b785d3817203f31f50b0830609bc771
Kevin-Ong <dragoN414@gmail.com>
2020-01-22 14:36:23 +0800

    Allow replays

--- src/minesweeper/MineSweeper.java
+++ src/minesweeper/MineSweeper.java
@@ -18,59 +18,71 @@ public class MineSweeper {
      */
     public static void main(String[] args) {
         int x, y, diff, row, col, mines;
+        boolean playAgain = true;
+        Board board;
         String action;
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Difficulty? (1-3) ");
-        diff = scanner.nextInt();
-        
-        if (diff == 1) {
-            row = 9;
-            col = 9;
-            mines = 10;
-        } else if (diff == 2) {
-            row = 16;
-            col = 16;
-            mines = 40;
-        } else if (diff == 3) {
-            row = 16;
-            col = 30;
-            mines = 99;
-        } else {
-            System.out.print("Custom: ");
-            row = scanner.nextInt();
-            col = scanner.nextInt();
-            mines = scanner.nextInt();
-        }
-        
-        Board board = new Board(row, col, mines);
-        board.setUp();
-        board.display(false);
-        
-        boolean lose = false, win = false;
-        
-        while (!lose && !win) {
+
+        while (playAgain) {
+            System.out.print("Difficulty? (1-3) ");
+            diff = scanner.nextInt();
+
+            if (diff == 1) {
+                row = 9;
+                col = 9;
+                mines = 10;
+            } else if (diff == 2) {
+                row = 16;
+                col = 16;
+                mines = 40;
+            } else if (diff == 3) {
+                row = 16;
+                col = 30;
+                mines = 99;
+            } else {
+                System.out.print("Custom: ");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+                mines = scanner.nextInt();
+            }
+
+            board = new Board(row, col, mines);
+            board.setUp();
+            board.display(false);
+
+            boolean lose = false, win = false;
+
+            while (!lose && !win) {
+                action = scanner.next();
+                x = scanner.nextInt();
+                y = scanner.nextInt();
+
+                if (action.equals("open")) {
+                    lose = !board.open(x, y);
+                    win = board.checkWin();
+                } else if (action.equals("flag")) {
+                    board.flag(x, y);
+                }
+
+                if (!win) {
+                    board.display(lose);
+                }
+
+                if (lose) {
+                    System.out.println("You Lose!");
+                } else if (win) {
+                    board.flagRest();
+                    board.display(lose);
+                    System.out.println("You Win!");
+                }
+            }
+
+            System.out.println();
+            System.out.print("Play again? (y/n) ");
             action = scanner.next();
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-            
-            if (action.equals("open")) {
-                lose = !board.open(x, y);
-                win = board.checkWin();
-            } else if (action.equals("flag")) {
-                board.flag(x, y);
-            }
-            
-            if (!win) {
-                board.display(lose);
-            }
-            
-            if (lose) {
-                System.out.println("You Lose!");
-            } else if (win) {
-                board.flagRest();
-                board.display(lose);
-                System.out.println("You Win!");
+
+            if (action.toLowerCase().equals("n")) {
+                playAgain = false;
             }
         }
     }
